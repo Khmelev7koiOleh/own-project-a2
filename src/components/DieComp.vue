d63bcaee2ba8a9aa97872bd1e008f2fe9d555933
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2024-09-23 21:51:36 +0200

    check screen issue 6redo

--- src/components/DieComp.vue
+++ src/components/DieComp.vue
@@ -12,20 +12,6 @@
       </div>
     </div>
   </div>
-
-  <div class="w-full h-auto gap-4 grid sm:flex">
-    <div class="flex items-center justify-between">
-      <div class="w-auto">
-        <div class="text-3xl font-bold p-2">{{ title }}</div>
-        <div class="text-gray-600 p-1">{{ text }}</div>
-        <div class="font-light text-sm text-gray-400">{{ subtext }}</div>
-        <button class="rounded-lg px-4 py-3 bg-green-700 my-6 text-white fond-semibold">
-          {{ button }}
-        </button>
-      </div>
-    </div>
-    <img :src="image" :width="size" alt="" class="rounded-md" />
-  </div>
 </template>
 
 <script setup>
