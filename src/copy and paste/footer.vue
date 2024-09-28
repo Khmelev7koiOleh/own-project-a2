2678fa85d2c7f3063092267cfd6aa377857cc119
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2024-09-28 17:00:05 +0200

    footer component 17o

--- src/copy and paste/footer.vue
+++ src/copy and paste/footer.vue
@@ -30,66 +30,11 @@
           <li class="text-base text-black font-light">McDonald’s Merchandise</li>
           <li class="text-base text-black font-light">Family Fun Hub</li>
           <li class="text-base text-black font-light">MyMcDonald's Rewards</li>
-          <li class="text-base text-black font-light">Investor Relations</li>
           <li class="text-base text-black font-light">McCafé®</li>
         </ul>
       </div>
-
-      <!-- Community Section -->
-      <div class="flex flex-col">
-        <div class="font-bold text-black text-base py-4">Community</div>
-        <ul>
-          <li class="text-base text-black font-light">About Us Overview</li>
-          <li class="text-base text-black font-light">Leadership Team</li>
-          <li class="text-base text-black font-light">Values In Action</li>
-          <li class="text-base text-black font-light">Franchising info</li>
-          <li class="text-base text-black font-light">Recalls & Alerts</li>
-          <li class="text-base text-black font-light">Real Estate</li>
-          <li class="text-base text-black font-light">Accessibility</li>
-          <li class="text-base text-black font-light">Investor Relations</li>
-          <li class="text-base text-black font-light">News & Notifications</li>
-          <li class="text-base text-black font-light">Something</li>
-          <li class="text-base text-black font-light">Something</li>
-          <li class="text-base text-black font-light">Something</li>
-        </ul>
-      </div>
-
-      <!-- Contact Us Section -->
-      <div class="flex flex-col">
-        <div class="font-bold text-black text-base py-4">Contact Us</div>
-        <ul>
-          <li class="text-base text-black font-light">Something</li>
-          <li class="text-base text-black font-light">About Us Overview</li>
-          <li class="text-base text-black font-light">Leadership Team</li>
-          <li class="text-base text-black font-light">Values In Action</li>
-          <li class="text-base text-black font-light">Franchising info</li>
-          <li class="text-base text-black font-light">Accessibility</li>
-          <li class="text-base text-black font-light">Investor Relations</li>
-          <li class="text-base text-black font-light">News</li>
-        </ul>
-      </div>
-    </div>
-
-    <!-- Social Media & App Download Icons -->
-    <div class="p-8"></div>
-    <div class="w-screen h-auto flex items-center justify-around">
-      <div class="flex items-center justify-around gap-4">
-        <img src="/img/facebook.png" alt="Facebook" />
-        <img src="/img/x.png" alt="X" />
-        <img src="/img/youtube.png" alt="YouTube" />
-        <img src="/img/instagram.png" alt="Instagram" />
-        <img src="/img/tumblr.png" alt="Tumblr" />
-        <img src="/img/spotify.png" alt="Spotify" />
-      </div>
-      <div class="flex items-center justify-around gap-8">
-        <img src="/img/app_store_badge.png" alt="App Store" />
-        <img src="/img/DownloadApp_Googleplay_154x40.png" alt="Google Play" />
-      </div>
     </div>
   </div>
-
-  <div class="border-b border-b-gray-300 py-8"></div>
-  <div class="p-8"></div>
 
   <!-- Mobile view -->
   <div class="w-screen h-full md:hidden">
@@ -106,7 +51,7 @@
           <li
             v-for="(item, index) in aboutUsItems"
             :key="index"
-            class="text-base text-black font-light py-2"
+            class="text-base text-black font-light"
           >
             {{ item }}
           </li>
@@ -125,7 +70,7 @@
           <li
             v-for="(item, index) in servicesItems"
             :key="index"
-            class="text-base text-black font-light py-2"
+            class="text-base text-black font-light"
           >
             {{ item }}
           </li>
@@ -144,7 +89,7 @@
           <li
             v-for="(item, index) in communityItems"
             :key="index"
-            class="text-base text-black font-light py-2"
+            class="text-base text-black font-light"
           >
             {{ item }}
           </li>
@@ -163,7 +108,7 @@
           <li
             v-for="(item, index) in contactItems"
             :key="index"
-            class="text-base text-black font-light py-2"
+            class="text-base text-black font-light"
           >
             {{ item }}
           </li>
