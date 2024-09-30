be50afe9e618a62b5571091740cadf0533abf244
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2024-09-30 21:05:58 +0200

    footer component 18ooooooo0ooo

--- src/components/SliderItem.vue
+++ src/components/SliderItem.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="max-w-300px grid md:hidden px-4">
-    <img width="300px" :src="slide.url" alt="" />
+  <div class="max-w-400px grid md:hidden px-4">
+    <img width="400px" :src="slide.url" alt="" />
     <div>{{ slide.song }}</div>
     <div>{{ slide.by }}</div>
-    <div>{{ slide.releasedOn }}</div>
+    <div class="text-red-400">{{ slide.releasedOn }}</div>
   </div>
 
   <div class="max-w-400px hidden md:flex md:flex-col md:items-center">
