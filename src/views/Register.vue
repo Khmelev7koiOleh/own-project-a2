a4951863a001e9076b964ea9a6fcb54309d1d6d7
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2024-10-06 21:47:48 +0200

    personal user account atempt 5 achieved ooooooooo

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -233,7 +233,7 @@ const linkGoogleAccount = async () => {
         </div>
       </div>
       <!-- Linking Form -->
-      <div v-if="openLinkform && !wotstScenario" class="bg-white py-8 px-4 rounded-2xl">
+      <div v-if="openLinkform && !wotstScenario" class="bg-white py-8 px-8 rounded-2xl">
         <div class="mt-6">
           <h3 class="text-lg font-bold text-gray-700 text-center">Link Google Account</h3>
           <div class="mb-4">
@@ -257,7 +257,7 @@ const linkGoogleAccount = async () => {
               class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
-          <div class="flex gap-2">
+          <div class="flex flex-col gap-4">
             <button
               type="button"
               @click="linkGoogleAccount"
@@ -278,7 +278,7 @@ const linkGoogleAccount = async () => {
       </div>
 
       <!-- Linking Form  worst scenario-->
-      <div v-if="openLinkform && wotstScenario" class="bg-white py-8 px-4 rounded-2xl">
+      <div v-if="openLinkform && wotstScenario" class="bg-white py-8 px-8 rounded-2xl">
         <div class="mt-6">
           <h3 class="text-lg font-bold text-gray-700 text-center">Link Google Account</h3>
           <div class="mb-4">
@@ -303,7 +303,7 @@ const linkGoogleAccount = async () => {
             />
           </div>
 
-          <div class="flex">
+          <div class="flex flex-col gap-4">
             <button
               type="button"
               @click="signUpWithGoogle"
